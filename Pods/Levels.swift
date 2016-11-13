156704ccb935365c20e02ad80321ce359a7dc7a4
Horacio Garza <hhgarza95@gmail.com>
2016-11-12 19:22:38 -0600

    Improvements 7:22p.m.

---
+++ Pods/Levels.swift
@@ -0,0 +1,16 @@
+//
+//  Levels.swift
+//  Pods
+//
+//  Created by Horacio Garza on 11/12/16.
+//
+//
+
+import Foundation
+import UIKit
+
+
+struct Level {
+    var levelImage:UIImageView
+    var isEnabled:Bool
+}
